0bbb5e92387fd47850fb0686724a590ded27fa66
Tamara Trefilova <tamara.trefilova@gmail.com>
2021-04-12 20:58:07 -0700

    first commit

--- src/components/DivElement.js
+++ src/components/DivElement.js
@@ -1,5 +1,16 @@
 // Import HTMLElement here
-
+import HTMLElement from './HTMLElement.js'
 // Define class here
 
+class DivElement extends HTMLElement {
+    constructor(content){
+        super(content);
+    }
+
+    render(){
+        return `<div> ${content} </div>`;
+    }
+}
+
 // Export class here
+export default class DivElement{};
--- src/components/HTMLElement.js
+++ src/components/HTMLElement.js
@@ -1,4 +1,17 @@
-// Define class here
 
+
+// import React, {Component} from 'react'
+// Define class here
+class HTMLElement {
+
+    constructor (tag, content){
+      this.tag = tag;
+      this.content = content;
+    }
+
+    render(){
+        return `<${tag}> ${content} </${tag}`;
+    }
+}
 // Export class here
-export default {};
+export default  class HTMLElement {};
